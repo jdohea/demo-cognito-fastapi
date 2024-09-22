35a26e9c7fda3740cd3bf9ff708841b1db578bf5
jdohea <jdohea@gmail.com>
2024-09-22 13:27:42 +0100

    minor updates

--- frontend/src/Auth/cognitoConfig.js
+++ frontend/src/Auth/cognitoConfig.js
@@ -1,4 +1,4 @@
 export const cognitoConfig = {
-  UserPoolId: "eu-west-1_MvfH7gb9W",  //import.meta.env.VITE_USER_POOL_ID,
-  ClientId: "1drqopaq5rmcr4akaksgu2np5c",
+  UserPoolId: import.meta.env.VITE_USER_POOL_ID,
+  ClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-require('dotenv').config();
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
